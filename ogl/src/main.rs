d4adbc3c0918afc92e5183ef9217ce08cbdd3456
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2021-12-02 19:47:22 -0300

    Hello world OpenGL

---
+++ ogl/src/main.rs
@@ -0,0 +1,66 @@
+#![allow(dead_code)]
+#![allow(unused)]
+use glutin::{ContextWrapper, PossiblyCurrent};
+use state::State;
+use winit::{event::*, event_loop::ControlFlow};
+
+mod helper;
+mod state;
+
+fn main() {
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let window = glutin::window::WindowBuilder::new();
+    let window = glutin::ContextBuilder::new()
+        .build_windowed(window, &event_loop)
+        .unwrap();
+
+    let window: ContextWrapper<PossiblyCurrent, glutin::window::Window> =
+        unsafe { window.make_current() }.unwrap();
+
+    let mut state: State = State::new(&window);
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent { ref event, .. } => {
+                if !state.input(event) {
+                    match event {
+                        WindowEvent::CloseRequested
+                        | WindowEvent::KeyboardInput {
+                            input:
+                                KeyboardInput {
+                                    state: ElementState::Pressed,
+                                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                                    ..
+                                },
+                            ..
+                        } => *control_flow = ControlFlow::Exit,
+                        WindowEvent::Resized(physical_size) => {
+                            state.resize(*physical_size);
+                        }
+                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                            // new_inner_size is &&mut so w have to dereference it twice
+                            state.resize(**new_inner_size);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            Event::RedrawRequested(_) => {
+                state.update();
+                match state.render() {
+                    Ok(_) => {
+                        window.swap_buffers().unwrap();
+                    }
+                    // // Reconfigure the surface if lost
+                    // Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                    // // The system is out of memory, we should probably quit
+                    // Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                    // // All other errors (Outdated, Timeout) should be resolved by the next frame
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            }
+            Event::RedrawEventsCleared => {}
+            _ => {}
+        }
+    });
+}
---
+++ ogl/src/state.rs
@@ -0,0 +1,89 @@
+use crate::helper::{compile_shader, link_program, VERTEX_DATA};
+use gl::types::*;
+use glutin::{ContextWrapper, PossiblyCurrent};
+use std::ffi::CStr;
+use std::mem;
+use std::ptr;
+use winit::event::*;
+
+pub struct State {}
+
+impl State {
+    // Creating some of the wgpu types requires async code
+    pub fn new(window: &ContextWrapper<PossiblyCurrent, glutin::window::Window>) -> Self {
+        gl::load_with(|symbol| window.get_proc_address(symbol));
+
+        let version = unsafe {
+            let data = CStr::from_ptr(gl::GetString(gl::VERSION) as *const _)
+                .to_bytes()
+                .to_vec();
+            String::from_utf8(data).unwrap()
+        };
+
+        println!("OpenGL version {}", version);
+
+        let vs_src = std::fs::read_to_string("shaders/gl_shader.vert")
+            .expect("Something went wrong reading vs_src");
+        let fs_src = std::fs::read_to_string("shaders/gl_shader.frag")
+            .expect("Something went wrong reading fs_src");
+
+        let vs = compile_shader(&vs_src, gl::VERTEX_SHADER);
+        let fs = compile_shader(&fs_src, gl::FRAGMENT_SHADER);
+        let program = link_program(vs, fs);
+
+        let mut vao = 0;
+        let mut vbo = 0;
+
+        unsafe {
+            // Create Vertex Array Object
+            gl::GenVertexArrays(1, &mut vao);
+            gl::BindVertexArray(vao);
+
+            // Create a Vertex Buffer Object and copy the vertex data to it
+            gl::GenBuffers(1, &mut vbo);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                mem::transmute(&VERTEX_DATA[0]),
+                gl::STATIC_DRAW,
+            );
+
+            // Use shader program
+            gl::UseProgram(program);
+            gl::BindFragDataLocation(program, 0, b"out_color\0".as_ptr() as *const _);
+
+            // Specify the layout of the vertex data
+            let pos_attr = gl::GetAttribLocation(program, b"position\0".as_ptr() as *const _);
+            gl::EnableVertexAttribArray(pos_attr as GLuint);
+            gl::VertexAttribPointer(
+                pos_attr as GLuint,
+                2,
+                gl::FLOAT,
+                gl::FALSE as GLboolean,
+                0,
+                ptr::null(),
+            );
+        }
+        Self {}
+    }
+
+    pub fn resize(&mut self, _new_size: winit::dpi::PhysicalSize<u32>) {}
+
+    pub fn input(&mut self, _event: &WindowEvent) -> bool {
+        false
+    }
+
+    pub fn update(&mut self) {}
+
+    pub fn render(&mut self) -> Result<(), ()> {
+        unsafe {
+            // Clear the screen to black
+            gl::ClearColor(0.5, 0.5, 0.5, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+            // Draw a triangle from the 3 vertices
+            // gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+        Ok(())
+    }
+}
